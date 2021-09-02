132622bf273a7e96d0527b0405875beec8c975fa
Victor Meireles <victordinami@gmail.com>
2021-09-02 20:39:25 -0300

    CORS, Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Class Validator
   app.useGlobalPipes(new ValidationPipe());
-  //
-  await app.listen(3000);
+  // CORS
+  app.enableCors();
+  // Helmet
+  app.use(helmet());
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
